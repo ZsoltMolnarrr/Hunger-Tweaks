20ed13cb66ae5f672a96db38afc984c1c0629e00
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-10-18 01:46:04 +0200

    Acutally update TinyConfig

--- common/src/main/java/net/hunger_tweaks/HungerTweaksMod.java
+++ common/src/main/java/net/hunger_tweaks/HungerTweaksMod.java
@@ -1,7 +1,7 @@
 package net.hunger_tweaks;
 
 import net.hunger_tweaks.config.Config;
-import net.tinyconfig.ConfigManager;
+import net.tiny_config.ConfigManager;
 
 public final class HungerTweaksMod {
     public static final String ID = "hunger_tweaks";
